b26349de9fe9b458efe1e326717cb707ad0de9cb
Aveek Saha <aveek.s98@gmail.com>
2021-09-23 21:22:08 +0530

    Split config and game

--- src/js/preload.js
+++ src/js/preload.js
@@ -1,26 +1,22 @@
-var Preload = function(game){};
+import 'phaser';
 
-Preload.prototype = {
+export default class PreloaderScene extends Phaser.Scene {
+  constructor () {
+    super('Preloader');
+  }
+  preload () {
 
-	preload: function(){ 
+	game.load.image('player', 'assets/player1.png');
+	game.load.image('ghost', 'assets/ghost.png');
+	game.load.image('dot', 'assets/dot.png');
+	game.load.spritesheet('pacman', 'assets/pacman.png', 32, 32);
 
-		// game.load.tilemap('soko', 'assets/soko.json', null, Phaser.Tilemap.TILED_JSON);
-		// game.load.image('tiles', 'assets/sokoban_tilesheet@2.png');
-		// game.load.image('player', 'assets/hulk.png');
-		// game.load.image('ground', 'assets/ground.png');
-		// game.load.image('wall', 'assets/ground_06.png');
-		game.load.image('player', 'assets/player1.png');
-		game.load.image('ghost', 'assets/ghost.png');
-		game.load.image('dot', 'assets/dot.png');
-		game.load.spritesheet('pacman', 'assets/pacman.png', 32, 32);
+	game.load.image('tiles', 'assets/sokoban_tilesheet.png');
+	game.load.image('wall', 'assets/wall.png');
+	game.load.tilemap('map', 'assets/map.json', null, Phaser.Tilemap.TILED_JSON);
 
-		game.load.image('tiles', 'assets/sokoban_tilesheet.png');
-		game.load.image('wall', 'assets/wall.png');
-		// this.load.spritesheet('pacman', 'assets/pacman.png', 32, 32);
-		game.load.tilemap('map', 'assets/map.json', null, Phaser.Tilemap.TILED_JSON);
-	},
-
-	create: function(){
-		this.game.state.start("Main");
-	}
-}
+  }
+  create () {
+	this.game.state.start("Main");
+  }
+};
