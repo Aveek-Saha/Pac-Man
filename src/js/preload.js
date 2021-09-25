d843babe16c5086292f0541d0bd70afb61dd3726
Aveek Saha <aveek.s98@gmail.com>
2021-09-25 08:58:18 +0530

    Update structure

--- src/js/preload.js
+++ src/js/preload.js
@@ -1,20 +1,20 @@
-var Preload = function(game){};
+// var Preload = function(game){};
 
-Preload.prototype = {
+// Preload.prototype = {
 
-	preload: function(){ 
+// 	preload: function(){ 
 
-		game.load.image('player', 'src/assets/player1.png');
-		game.load.image('ghost', 'src/assets/ghost.png');
-		game.load.image('dot', 'src/assets/dot.png');
-		game.load.spritesheet('pacman', 'src/assets/pacman.png', 32, 32);
+// 		game.load.image('player', 'src/assets/player1.png');
+// 		game.load.image('ghost', 'src/assets/ghost.png');
+// 		game.load.image('dot', 'src/assets/dot.png');
+// 		game.load.spritesheet('pacman', 'src/assets/pacman.png', 32, 32);
 
-		game.load.image('tiles', 'src/assets/sokoban_tilesheet.png');
-		game.load.image('wall', 'src/assets/wall.png');
-		game.load.tilemap('map', 'src/assets/map.json', null, Phaser.Tilemap.TILED_JSON);
-	},
+// 		game.load.image('tiles', 'src/assets/sokoban_tilesheet.png');
+// 		game.load.image('wall', 'src/assets/wall.png');
+// 		game.load.tilemap('map', 'src/assets/map.json', null, Phaser.Tilemap.TILED_JSON);
+// 	},
 
-	create: function(){
-		this.game.state.start("Main");
-	}
-}
+// 	create: function(){
+// 		this.game.state.start("Main");
+// 	}
+// }
